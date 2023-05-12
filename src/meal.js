e9f46285a7743c001ea19aedfbd0fccd07f34d75
Natalie Rodriguez <codinggeographer@gmail.com>
2023-05-11 21:04:21 -0600

    unskip createMenuItem tests / add associated function;

--- src/meal.js
+++ src/meal.js
@@ -2,10 +2,18 @@ function nameMenuItem(name) {
   return `Delicious ${name}`
 }
 
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type
+  }
+  return menuItem
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
+  createMenuItem,
   // addIngredients,
   // formatPrice,
   // decreasePrice,
