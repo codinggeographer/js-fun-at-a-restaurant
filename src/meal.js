83b9f59583b5550952b2354055bb7fd4df111d6a
Natalie Rodriguez <codinggeographer@gmail.com>
2023-05-12 08:58:23 -0600

    unskip tests: create functions

--- src/meal.js
+++ src/meal.js
@@ -22,13 +22,26 @@ function formatPrice(initialPrice) {
   return `$${initialPrice}`
 }
 
+function decreasePrice(price) {
+  price = price - price * .1
+  return price
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+  return recipe 
+}
 module.exports = {
   nameMenuItem,
   createMenuItem,
   addIngredients,
   formatPrice,
-  // decreasePrice,
-  // createRecipe
+  decreasePrice,
+  createRecipe
 }
 
 
