eb336acb06f4dae9a76e7b76a23c3be87ef95490
Natalie Rodriguez <codinggeographer@gmail.com>
2023-05-12 18:09:56 -0600

    Unskip tests: red-green testing: create functions

--- src/order.js
+++ src/order.js
@@ -1,7 +1,31 @@
+function takeOrder(newOrder, deliveryOrders) {
+  if(deliveryOrders.length < 3) {
+    deliveryOrders.push(newOrder)
+  }
+  return deliveryOrders
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+  var index = deliveryOrders.findIndex(order => order['orderNumber'] === orderNumber)
+  if (index !== -1) {
+    deliveryOrders.splice(index, 1)
+  }
+  return deliveryOrders
+}
+
+function listItems(deliveryOrders) {
+  var itemList = deliveryOrders.map(order => order.item)
+  return itemList.join(", ")
+}
+
+function searchOrder(deliveryOrders, item) {
+  var orderItems = listItems(deliveryOrders)
+  return orderItems.includes(item)
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
